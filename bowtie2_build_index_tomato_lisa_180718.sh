06c13a90281f133aafb3a2f702226096aeb42a68
tijsbliek <bliek@uva.nl>
2018-07-19 16:33:04 +0200

    tomato to petunia
    indexing changed to petunia

--- bowtie2_build_index_tomato_lisa_180718.sh
+++ bowtie2_build_index_tomato_lisa_180718.sh
@@ -12,7 +12,8 @@ module load bowtie/2.2.4
 cp "${HOME}"/genomes/S_lycopersicum_chromosomes.3.00.fa "${TMPDIR}"/
 
 #build index from the reference genome
-bowtie2-build S_lycopersicum_chromosomes.3.00.fa tomato_inx
+bowtie2-build petunia_genome tomato_inx
+
 # bowtie-build [reference fasta] [base name for index file]
 
 mv "${TMPDIR}"/* "${HOME}"/genomes/
